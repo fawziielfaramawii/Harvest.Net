54184ff9388d578f4c6ebe1d6f35ebcaa13182df
KareemAymann <126060957+KareemAymann@users.noreply.github.com>
2024-08-30 07:12:50 +0300

    EFCProject

---
+++ Karim_Ayman/EFC_Project/EFC_Project/Configurations/DepartmentEntityTypeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using EFC_Project.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFC_Project.Configurations
+{
+    public class DepartmentEntityTypeConfiguration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            //we cant do it in data annotation
+            //builder.HasKey(a => new {a.Name,a.Location});
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Karim_Ayman/EFC_Project/EFC_Project/Configurations/EmployeeEntityTypeConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using EFC_Project.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFC_Project.Configurations
+{
+    public class EmployeeEntityTypeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+
+            builder.HasKey(a => a.EmpId);
+
+
+
+
+            builder.Property(a => a.Name)
+                .IsRequired()
+                .HasMaxLength(150)
+                .HasColumnName("Full Name");
+
+            builder.Property(a => a.Salary)
+                .IsRequired()
+                .HasDefaultValue(5000);
+
+
+
+            builder
+         .Property(e => e.Age)
+         .HasComputedColumnSql("DATEDIFF(year, [DateOfBirth], GETDATE())");
+
+        }
+    } 
+}
